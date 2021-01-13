f2dfaadf20f3e458a3732cd5e4479ea53e61fa46
mr-yanghm <code004@gmail.com>
2021-01-13 15:16:52 +0900

    templete default

--- src/app.jsx
+++ src/app.jsx
@@ -1,28 +1,9 @@
-import React, { useEffect, useState } from "react";
-import "./app.css";
-import VideoList from "./components/video_list/video_list";
+import React, { Component } from "react";
 
-function App() {
-  const [videos, setVideos] = useState([]);
-
-  useEffect(() => {
-    const requestOptions = {
-      method: "GET",
-      redirect: "follow",
-    };
-
-    fetch(
-      "https://youtube.googleapis.com/youtube/v3/videos?part=snippet&chart=mostPopular&maxResults=25&key=AIzaSyAU1YyXdo8gO7kYc0Ypy0f66VBGja-8ofU",
-      requestOptions
-    )
-      .then((response) => response.json())
-      .then((result) => setVideos(result.items))
-      .catch((error) => console.log("error", error));
-
-    console.log("useEffect");
-  }, []);
-
-  return <VideoList videos={videos} />;
+class app extends Component {
+  render() {
+    return <div>Hello</div>;
+  }
 }
 
-export default App;
+export default app;
