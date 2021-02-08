8b4b0f3918521930a870c0162ea7703296582033
yanghm <code004@gmail.com>
2021-02-08 19:06:42 +0900

    전용면적, 공급면적 계산

--- src/app.jsx
+++ src/app.jsx
@@ -4,11 +4,23 @@ import AptInfo from './components/AptInfo';
 const App = ({ openApi }) => {
   const [aptInfo, setAptInfo] = useState({});
   const search = (query) => {
-    openApi
+    return openApi
       .search(query || '202101')
       .then((data) => {
-        console.log(data.items.item.filter((data) => {
+
+        const allData = data.items.item.filter((data) => {
           return data.아파트 === '율곡'
+        });
+
+        console.log(allData);
+        const sizeArr = allData.map((data) => {
+          return data.전용면적;
+        })
+
+        console.log(new Set(sizeArr));
+
+        console.log([...new Set(sizeArr)].map((data) => {
+          return data / 3.3;
         }));
       }
       );
